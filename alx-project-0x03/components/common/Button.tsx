aee7d6650864cd666cd31da73226af3df471754a
Talaku Getahun <getahuntalaku@gmail.com>
2025-01-12 21:23:57 +0300

    after content of interface/index.tsx, pages/index.tsx, components/common/Button.tsx, components/layouts/Layout.tsx is updated

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,13 +1,7 @@
 
 
-interface ButtonProps {
-    buttonLabel: string
-    buttonSize?: string
-    buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-    action?: () => void
-  }
-  
-  
+import { ButtonProps } from "@/interface";
+
   const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
   
     const backgroundColorClass = buttonBackgroundColor ? {
--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -4,10 +4,7 @@ import Button from "@/components/common/Button";
 import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
 import { useRouter } from "next/router";
-
-interface PageRouteProps {
-  pageRoute: string
-}
+import { PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter()
@@ -18,8 +15,6 @@ export default function Home() {
   }
 
   return (
-    <>
-    <Header />
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
@@ -37,7 +32,6 @@ export default function Home() {
         <Button action={() => routeToNextPage({ pageRoute: '/counter-app'})} buttonLabel="Contact us" buttonBackgroundColor="orange" />
       </div>
     </div>
-    <Footer />
-    </>
+
   );
 }
